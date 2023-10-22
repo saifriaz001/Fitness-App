9261266fe8c217817d231f6a8eaf9de76c4d5353
saifriaz001 <89684150+saifriaz001@users.noreply.github.com>
2023-10-22 16:50:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+
+function App() {
+  return (
+    <div className= "bg-blue-950">
+      <div className="   bg-blue-950 ">
+        <div className="w-[1000px] h-[1000px]  mx-auto bg-blue-950  ">
+
+        <div className=" mt-10 w-[300px] h-[600px]  border-4 border-gray-700 mx-auto  rounded-sm  bg-white">
+          <div className=" flex flex-row gap-2  mt-2  items-end ">
+          <p className="   ml-60 text-gray-600 " ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+  <path fill-rule="evenodd" d="M10 2a6 6 0 00-6 6c0 1.887-.454 3.665-1.257 5.234a.75.75 0 00.515 1.076 32.91 32.91 0 003.256.508 3.5 3.5 0 006.972 0 32.903 32.903 0 003.256-.508.75.75 0 00.515-1.076A11.448 11.448 0 0116 8a6 6 0 00-6-6zM8.05 14.943a33.54 33.54 0 003.9 0 2 2 0 01-3.9 0z" clip-rule="evenodd" />
+</svg>
+</p>
+            <p className="text-gray-600" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clipRule="evenodd" />
+</svg>
+</p>
+
+          </div>
+          <div className=" flex flex-col mt-5 items-baseline relative z-0">
+            <p className=" text-2xl font-bold  ">Store</p>
+            <p className=" mt-2 ml-2 w-[270px] border rounded-lg py-7 px-4  text-lg text-extrabold  bg-blue-700 text-white ">Meal Plan</p>
+            <p className=" mt-2 ml-2 w-[270px] border rounded-lg py-7 px-4  text-lg text-extrabold bg-pink-600 text-white ">Training plan</p>
+            <p  className="mt-2 ml-2 w-[270px]  border rounded-lg py-7 px-4 text-lg text-extrabold  bg-purple-600 text-white ">Supplement routine</p>
+            <p  className="mt-2 ml-2 w-[270px]  border rounded-lg py-7 px-4 text-lg text-extrabold bg-gray-700 text-white ">Track Progress</p>
+            
+          </div>
+          <div className=" p-[0.5px] text-gray-600 bg-gray-600 mt-24 w-[292px]"></div>
+          <div className="flex flex-row items-center justify-evenly  mt-2  ">
+            <p className=" text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+</p>
+            <p className=" text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+</svg>
+</p>
+            <p className=" text-gray-700" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+</svg>
+
+</p>
+            <p className=" text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+</svg>
+</p>
+            <p className=" text-gray-700"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+</p>
+          </div>
+        </div> 
+
+        </div>
+        
+
+      </div>
+
+    </div>
+    
+       
+      )
+}
+
+export default App;
